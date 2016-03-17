42d7dcec8be3df08ca49e713e13715d9873d5f6e
drisoftie <drisoftie@gmail.com>
2016-03-17 12:12:17 +0100

    study preps

--- app/src/main/java/de/uni_stuttgart/mci/bluecon/fragments/BlueconPageAdapter.java
+++ app/src/main/java/de/uni_stuttgart/mci/bluecon/fragments/BlueconPageAdapter.java
@@ -4,35 +4,31 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
-import de.uni_stuttgart.mci.bluecon.fragments.ScanListFragment;
-import de.uni_stuttgart.mci.bluecon.fragments.SearchListFragment;
-import de.uni_stuttgart.mci.bluecon.fragments.NavigationListFragment;
-
 /**
  * Created by florian on 22.10.15.
  */
 public class BlueconPageAdapter extends FragmentPagerAdapter {
-    ScanListFragment scanFrag;
-    SearchListFragment searchFrag;
+    ScanListFragment       scanFrag;
+    SearchListFragment     searchFrag;
     NavigationListFragment naviFrag;
 
-    public BlueconPageAdapter (FragmentManager fm) {
+    public BlueconPageAdapter(FragmentManager fm) {
         super(fm);
-//        scanFrag = new ScanListFragment();
+        //        scanFrag = new ScanListFragment();
         scanFrag = new ScanListFragment();
         searchFrag = new SearchListFragment();
         naviFrag = new NavigationListFragment();
     }
 
     @Override
-    public Fragment getItem (int i) {
-        switch (i){
+    public Fragment getItem(int i) {
+        switch (i) {
             case 0:
-                return scanFrag;
-            case 1:
-                return searchFrag;
-            case 2:
                 return naviFrag;
+            //            case 1:
+            //                return searchFrag;
+            //            case 2:
+            //                return scanFrag;
             default:
                 return null;
         }
@@ -41,18 +37,20 @@ public class BlueconPageAdapter extends FragmentPagerAdapter {
     @Override
     public CharSequence getPageTitle(int position) {
 
-        switch (position){
+        switch (position) {
             case 0:
-                return "Scan";
-            case 1:
-                return "Search";
-            case 2:
                 return "Navigation";
+            //            case 1:
+            //                return "Suche";
+            //            case 2:
+            //                return "Scan";
             default:
                 return null;
-    }
+        }
     }
 
     @Override
-    public int getCount() {return 3;}
+    public int getCount() {
+        return 1;
+    }
 }
--- app/src/main/java/de/uni_stuttgart/mci/bluecon/fragments/NavigationListFragment.java
+++ app/src/main/java/de/uni_stuttgart/mci/bluecon/fragments/NavigationListFragment.java
@@ -1,14 +1,17 @@
 package de.uni_stuttgart.mci.bluecon.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.accessibility.AccessibilityEvent;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,14 +21,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import de.uni_stuttgart.mci.bluecon.BeaconHolder;
-import de.uni_stuttgart.mci.bluecon.domain.BeaconLocation;
 import de.uni_stuttgart.mci.bluecon.R;
+import de.uni_stuttgart.mci.bluecon.domain.BeaconLocation;
 import de.uni_stuttgart.mci.bluecon.domain.RangeThreshold;
 import de.uni_stuttgart.mci.bluecon.ui.BeaconsNaviAdapter;
 import de.uni_stuttgart.mci.bluecon.ui.BeaconsViewHolder;
 import de.uni_stuttgart.mci.bluecon.util.IResultListener;
+import de.uni_stuttgart.mci.bluecon.util.ScheduleUtil;
 
 public class NavigationListFragment extends Fragment implements BeaconHolder.IBeaconListener {
     // TODO: Rename parameter arguments, choose names that match
@@ -37,18 +42,18 @@ public class NavigationListFragment extends Fragment implements BeaconHolder.IBe
     private String mParam1;
     private String mParam2;
 
-    private Button bt_from;
-    private Button bt_to;
-    private Button bt_calc;
+    private Button   bt_from;
+    private Button   bt_to;
+    private Button   bt_calc;
     private TextView fromText;
     private TextView toText;
 
-    private BeaconLocation start = null;
-    private BeaconLocation target = null;
+    private BeaconLocation       start      = null;
+    private BeaconLocation       target     = null;
     private List<BeaconLocation> resultList = new ArrayList<>();
 
 
-    private RecyclerView mRecyclerView;
+    private RecyclerView       mRecyclerView;
     private BeaconsNaviAdapter mAdapter;
 
 
@@ -67,7 +72,7 @@ public class NavigationListFragment extends Fragment implements BeaconHolder.IBe
     // TODO: Rename and change types and number of parameters
     public static NavigationListFragment newInstance(String param1, String param2) {
         NavigationListFragment fragment = new NavigationListFragment();
-        Bundle args = new Bundle();
+        Bundle                 args     = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
@@ -82,8 +87,7 @@ public class NavigationListFragment extends Fragment implements BeaconHolder.IBe
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.navigation_list_fragment, container, false);
         bt_from = (Button) v.findViewById(R.id.bt_from);
         bt_to = (Button) v.findViewById(R.id.bt_to);
@@ -96,22 +100,22 @@ public class NavigationListFragment extends Fragment implements BeaconHolder.IBe
         initRecyclerView(mRecyclerView);
 
 
-//        mRecyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
-//            //Does nothing :)
-//            @Override
-//            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-//                super.onScrollStateChanged(recyclerView, newState);
-//            }
-//
-//            // Enables Swiping only when at Top of the list
-//            @Override
-//            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-//                boolean enable = false;
-//                if (mRecyclerView != null && mRecyclerView.getChildCount() > 0) {
-//                    enable = mRecyclerView.getChildAt(0).getTop() == 0;
-//                }
-//            }
-//        });
+        //        mRecyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+        //            //Does nothing :)
+        //            @Override
+        //            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+        //                super.onScrollStateChanged(recyclerView, newState);
+        //            }
+        //
+        //            // Enables Swiping only when at Top of the list
+        //            @Override
+        //            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+        //                boolean enable = false;
+        //                if (mRecyclerView != null && mRecyclerView.getChildCount() > 0) {
+        //                    enable = mRecyclerView.getChildAt(0).getTop() == 0;
+        //                }
+        //            }
+        //        });
 
 
         mAdapter = new BeaconsNaviAdapter(resultList);
@@ -133,6 +137,17 @@ public class NavigationListFragment extends Fragment implements BeaconHolder.IBe
                     public void onResult(BeaconLocation result) {
                         start = result;
                         fromText.setText(result.placeId);
+                        ScheduleUtil.scheduleWork(new Runnable() {
+                            @Override
+                            public void run() {
+                                getActivity().runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        bt_to.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_FOCUSED);
+                                    }
+                                });
+                            }
+                        }, 500, TimeUnit.MILLISECONDS);
                     }
                 }).show(getActivity().getSupportFragmentManager(), "dialog");
             }
@@ -146,6 +161,17 @@ public class NavigationListFragment extends Fragment implements BeaconHolder.IBe
                     public void onResult(BeaconLocation result) {
                         target = result;
                         toText.setText(result.placeId);
+                        ScheduleUtil.scheduleWork(new Runnable() {
+                            @Override
+                            public void run() {
+                                getActivity().runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        bt_calc.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_FOCUSED);
+                                    }
+                                });
+                            }
+                        }, 500, TimeUnit.MILLISECONDS);
                     }
                 }).show(getActivity().getSupportFragmentManager(), "dialog");
             }
@@ -156,7 +182,7 @@ public class NavigationListFragment extends Fragment implements BeaconHolder.IBe
             public void onClick(View v) {
                 if (start != null && target != null) {
                     BeaconHolder.inst().resetRSSI();
-                    new CalculateShortestPath().execute(start, target);
+                    new CalculateShortestPath().execute();
                 } else {
 
                     Toast.makeText(getActivity(), R.string.txt_navi_error_no_two_selected, Toast.LENGTH_SHORT).show();
@@ -175,31 +201,42 @@ public class NavigationListFragment extends Fragment implements BeaconHolder.IBe
 
     @Override
     public void onBeaconChanged(BeaconLocation changedBeacon) {
-        BeaconsViewHolder viewHolder = (BeaconsViewHolder) mRecyclerView.findViewHolderForAdapterPosition(mAdapter.getBeaconsList().indexOf(changedBeacon));
-        if (viewHolder != null && viewHolder.vRSSI != null) {
-            if (changedBeacon.RSSI < RangeThreshold.NEAR) {
-                viewHolder.vRSSI.setText(R.string.txt_very_close);
-            } else if (changedBeacon.RSSI < RangeThreshold.MIDDLE) {
-                viewHolder.vRSSI.setText(R.string.txt_near);
-            } else if (changedBeacon.RSSI < RangeThreshold.FAR) {
-                viewHolder.vRSSI.setText(R.string.txt_in_range);
-            } else {
-                viewHolder.vRSSI.setText(R.string.txt_out_of_range);
+        for (BeaconLocation b : resultList) {
+            if (b.macAddress.equals(changedBeacon.macAddress)) {
+                BeaconsViewHolder viewHolder = (BeaconsViewHolder) mRecyclerView.findViewHolderForAdapterPosition(
+                        mAdapter.getBeaconsList().indexOf(changedBeacon));
+                if (viewHolder != null && viewHolder.vRSSI != null) {
+                    if (changedBeacon.RSSI < RangeThreshold.NEAR) {
+                        viewHolder.vRSSI.setText(R.string.txt_very_close);
+                    } else if (changedBeacon.RSSI < RangeThreshold.MIDDLE) {
+                        viewHolder.vRSSI.setText(R.string.txt_near);
+                    } else if (changedBeacon.RSSI < RangeThreshold.FAR) {
+                        viewHolder.vRSSI.setText(R.string.txt_in_range);
+                    } else {
+                        viewHolder.vRSSI.setText(R.string.txt_out_of_range);
+                    }
+                    //            viewHolder.vRSSI_details.setText(String.valueOf(changedBeacon.RSSI));
+
+                }
+                if (changedBeacon.preRSSI < RangeThreshold.NEAR && changedBeacon.RSSI >= RangeThreshold.NEAR) {
+                    changedBeacon.preRSSI = changedBeacon.RSSI;
+                    Toast.makeText(getActivity(), changedBeacon.placeId + this.getString(R.string.txt_is_very_close), Toast.LENGTH_SHORT)
+                         .show();
+                } else if (changedBeacon.preRSSI < RangeThreshold.MIDDLE && changedBeacon.RSSI >= RangeThreshold.MIDDLE) {
+                    changedBeacon.preRSSI = changedBeacon.RSSI;
+                    Toast.makeText(getActivity(), changedBeacon.placeId + this.getString(R.string.txt_is_near), Toast.LENGTH_SHORT).show();
+                    LocalBroadcastManager.getInstance(getActivity()).sendBroadcast(new Intent(getString(R.string.intent_gatt_open))
+                                                                                           .putExtra(getString(R.string.bndl_mac),
+                                                                                                     changedBeacon.macAddress));
+                } else if (changedBeacon.preRSSI < RangeThreshold.FAR && changedBeacon.RSSI >= RangeThreshold.FAR) {
+                    changedBeacon.preRSSI = changedBeacon.RSSI;
+                    Toast.makeText(getActivity(), changedBeacon.placeId + this.getString(R.string.txt_is_in_range), Toast.LENGTH_SHORT)
+                         .show();
+                }
+
+                break;
             }
-//            viewHolder.vRSSI_details.setText(String.valueOf(changedBeacon.RSSI));
-
         }
-        if (changedBeacon.preRSSI < RangeThreshold.NEAR && changedBeacon.RSSI >= RangeThreshold.NEAR) {
-            changedBeacon.preRSSI = changedBeacon.RSSI;
-            Toast.makeText(getActivity(), changedBeacon.placeId + this.getString(R.string.txt_is_very_close), Toast.LENGTH_SHORT).show();
-        } else if (changedBeacon.preRSSI < RangeThreshold.MIDDLE && changedBeacon.RSSI >= RangeThreshold.MIDDLE) {
-            changedBeacon.preRSSI = changedBeacon.RSSI;
-            Toast.makeText(getActivity(), changedBeacon.placeId + this.getString(R.string.txt_is_near), Toast.LENGTH_SHORT).show();
-        } else if (changedBeacon.preRSSI < RangeThreshold.FAR && changedBeacon.RSSI >= RangeThreshold.FAR) {
-            changedBeacon.preRSSI = changedBeacon.RSSI;
-            Toast.makeText(getActivity(), changedBeacon.placeId + this.getString(R.string.txt_is_in_range), Toast.LENGTH_SHORT).show();
-        }
-
     }
 
     @Override
@@ -212,6 +249,25 @@ public class NavigationListFragment extends Fragment implements BeaconHolder.IBe
 
     }
 
+    private void initRecyclerView(RecyclerView recyclerView) {
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setDescendantFocusability(ViewGroup.FOCUS_AFTER_DESCENDANTS);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(mLayoutManager);
+        ((LinearLayoutManager) mLayoutManager).setOrientation(LinearLayoutManager.VERTICAL);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
     // BFS: One Level of the Search tree after the other will be searched fully.
     // future work: for bigger Databases Dijkstra or Floyd Warshall to calculate all shortest paths first
     private class CalculateShortestPath extends AsyncTask<BeaconLocation, Void, List<BeaconLocation>> {
@@ -220,8 +276,6 @@ public class NavigationListFragment extends Fragment implements BeaconHolder.IBe
 
         @Override
         protected List<BeaconLocation> doInBackground(BeaconLocation[] params) {
-            start = params[0];
-            target = params[1];
             allBeacons = BeaconHolder.beaconLocations();
             Map<String, BeaconLocation> allBeaconsMap = new HashMap<>();
             for (BeaconLocation aB : allBeacons) {
@@ -237,7 +291,7 @@ public class NavigationListFragment extends Fragment implements BeaconHolder.IBe
             List<SearchObject> oneLevel = new ArrayList<>();
             oneLevel.add(startObj);
             List<SearchObject> next = new ArrayList<>();
-            boolean stop = false;
+            boolean            stop = false;
             while (!stop) {
                 for (SearchObject ol : oneLevel) {
                     Set<String> neighbors = ol.active.neighborhood.keySet();
@@ -252,15 +306,16 @@ public class NavigationListFragment extends Fragment implements BeaconHolder.IBe
                             }
                             nextObj.path.add(nextObj.active);
                             next.add(nextObj);
-//                            Log.d("NaviFrag", "added" + nextObj.active.roomId);
+                            //                            Log.d("NaviFrag", "added" + nextObj.active.roomId);
                         }
                         if (target.roomId.equals(n)) {
                             stop = true;
                             break;
                         }
                     }
-                    if (stop)
+                    if (stop) {
                         break;
+                    }
                 }
                 oneLevel.clear();
                 for (SearchObject n : next) {
@@ -286,33 +341,16 @@ public class NavigationListFragment extends Fragment implements BeaconHolder.IBe
             mAdapter.getBeaconsList().addAll(resultList);
             mAdapter.notifyDataSetChanged();
 
+            mRecyclerView.setVisibility(View.VISIBLE);
+
             Toast.makeText(getActivity(), R.string.txt_navi_toast_calc_succ, Toast.LENGTH_SHORT).show();
             return;
         }
 
         private class SearchObject {
-            public BeaconLocation active;
-            public BeaconLocation pre;
+            public BeaconLocation       active;
+            public BeaconLocation       pre;
             public List<BeaconLocation> path;
         }
-    }
-
-    private void initRecyclerView(RecyclerView recyclerView) {
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setDescendantFocusability(ViewGroup.FOCUS_AFTER_DESCENDANTS);
-
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
-        recyclerView.setLayoutManager(mLayoutManager);
-        ((LinearLayoutManager) mLayoutManager).setOrientation(LinearLayoutManager.VERTICAL);
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
     }
 }
